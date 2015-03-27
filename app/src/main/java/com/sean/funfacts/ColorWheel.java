42ba0b0430b24703d0414d94e87022cc0bf7ba3b
Sean Kelley <s.kelley27@gmail.com>
2015-03-27 14:11:05 -0500

    Base App Complete

---
+++ app/src/main/java/com/sean/funfacts/ColorWheel.java
@@ -0,0 +1,32 @@
+package com.sean.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by snkelley on 3/27/2015.
+ *
+ */
+public class ColorWheel {
+    String[] mColor = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+    private Random randomGenerator = new Random();
+
+    public int getColor(){
+        return Color.parseColor(mColor[randomGenerator.nextInt(mColor.length)]);
+    }
+}
--- app/src/main/java/com/sean/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/sean/funfacts/FunFactsActivity.java
@@ -1,17 +1,42 @@
 package com.sean.funfacts;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class FunFactsActivity extends ActionBarActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //declare view variables
+        final RelativeLayout factsLayout = (RelativeLayout) findViewById(R.id.factLayout);
+        final TextView factLabel = (TextView)findViewById(R.id.factTextView);
+        final Button showFactButton = (Button)findViewById(R.id.showFactButton);
+
+        showFactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ColorWheel mColorWheel = new ColorWheel();
+                int color = mColorWheel.getColor();
+                factsLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+                FactBook facts = new FactBook();
+                factLabel.setText(facts.getFact());
+            }
+        });
+        //Toast.makeText(this, "Toast example", Toast.LENGTH_LONG).show();
+        Log.i(TAG, "Activity Started");
     }
 
 
